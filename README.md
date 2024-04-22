7063d12713e3fa77a0d5a078b1ba0e5f215eeba5
lat0z <39180797+lat0z@users.noreply.github.com>
2024-04-22 03:23:47 +0000

    add solutions

---
+++ README.md
@@ -0,0 +1,4 @@
+Proposed solution for task 2
+
+Create a workflow for the most common tasks, from the example to automate the solution for the most common issue related to EndpointRegistrationTransientFailure, without requiring a human intervention. That way the engineers release from the toil related to solve that recurring issue.
+An example of a tool might be qntrl 
--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,16 @@
-#TODO
+#/bin/bash
+export NAMESPACE_NAME=sre
+export DEPLOYMENT_NAME=swype-app
+MAX_RESTARTS_ALLOWED=3
+while [ true  ]
+do  
+    result=$(kubectl get pods -n $NAMESPACE_NAME --no-headers | grep $DEPLOYMENT_NAME | awk '{print $4}')
+    echo "Restart Counter for deployment $DEPLOYMENT_NAME - $result"
+    if [ $result -le $MAX_RESTARTS_ALLOWED ]
+    then
+        sleep 1m
+    else
+        kubectl scale --replicas=0 deployment/$DEPLOYMENT_NAME -n $NAMESPACE_NAME
+        break
+    fi
+done
